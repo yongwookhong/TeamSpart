0f150d3aada3101129a699c0c64d3b41146e9a48
yomgoun <ygy718500@gmail.com>
2024-01-09 14:51:32 +0900

    플레이어1

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@
 
 class Player
 {
-    int lv = 1;
-    string job = "전사";
+    int Lv = 1;
+    string Job = "전사";
     float Atk = 10;
     float Def = 5;
     float Hp = 100;
@@ -20,7 +20,20 @@ class Player
 
     public void Status()
     {
-       
+        Console.WriteLine("상태보기");
+        Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+        Console.WriteLine("");
+        Console.WriteLine($"Lv. {Lv}");
+        Console.WriteLine($"Chad({Job})");
+        Console.WriteLine($"공격력:{Atk}");
+        Console.WriteLine($"방어력:{Def}");
+        Console.WriteLine($"체력:{Hp}");
+        Console.WriteLine($"Gold:{Gold}");
+        Console.WriteLine("");
+        Console.WriteLine("0. 나가기");
+        Console.WriteLine("");
+        Console.WriteLine("원하시는 행동을 입력해 주세요.");
+        Console.Write(">>");
     }
     
 }
