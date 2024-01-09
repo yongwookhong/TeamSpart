23422915bf0674462170bf01a7a6ac6331468317
yomgoun <ygy718500@gmail.com>
2024-01-09 14:36:27 +0900

    플레이어

--- Program.cs
+++ Program.cs
@@ -2,7 +2,25 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("안새새55");
+        
     }
 }
 
+class Player
+{
+    int lv = 1;
+    string job = "전사";
+    float Atk = 10;
+    float Def = 5;
+    float Hp = 100;
+    int Gold = 1500;
+    float M_Hp ;
+    float Exp ;
+    float M_Exp ;
+
+    public void Status()
+    {
+       
+    }
+    
+}
